d1aafff3abe5d5d64f9ca2158ad23d03e70b8ed4
Adrienne <iliu.adrienne@gmail.com>
2019-07-25 22:31:41 -0700

    render images onto page

--- src/App.js
+++ src/App.js
@@ -1,19 +1,50 @@
 import React, {Component} from 'react';
 import './App.css';
-import foods from "./foods.json";
+import burgers from "./burgers.json";
+import Wrapper from "./components/Wrapper/index";
+import BurgerCard from "./components/BurgerCard/index";
+
+
+//TODO: 
+// navbar
+//    li: [title, click], [you guessed in/correct], [score/high score]
+// header with brief instructions
+// pictures area
+// bottom div
+// footer
 
 class App extends Component {
 
   //Set this.state.food to json array
   state = {
-    foods
+    burgers
+  };
+
+  removeBurger = id => {
+    // Filter this.state.burgers for characters with an id not equal to the id being removed
+    const burgers = this.state.burgers.filter(burger => burger.id !== id);
+    // Set this.state.burgers equal to the new burgers array
+    this.setState({ burgers });
   };
 
   render(){
   return (
-    <div className="App">
-        <h1>Hello</h1>
-    </div>
+    <Wrapper>
+      <div className="App">
+          <h1>Hello</h1>
+
+          {this.state.burgers.map(burger => (
+          <BurgerCard
+          removeBurger={this.removeBurger}
+          id={burger.id}
+          key={burger.id}
+          name={burger.name}
+          image={burger.image}
+          />
+          ))}
+
+      </div>
+    </Wrapper>
   );
 }
 }
